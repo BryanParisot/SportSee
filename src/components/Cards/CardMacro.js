0e0ec85511c574b3f027bb746838f71c4d7f7df9
Bryan <bryanparisot@hotmail.com>
2022-07-29 14:34:43 +0200

    :sparkles: create the component card-macro

---
+++ src/components/Cards/CardMacro.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const CardMacro = (props) => {
+  return (
+    <div className="CardMacro">
+      <div  className={`${props.bgColor} contain_logo`}>
+        <img src={props.logo} alt="logo" />
+      </div>
+      <div className="contain_informations">
+        <p className="title">{props.title}</p>
+        <p className="weight">{props.weight}</p>
+      </div>
+    </div>
+  );
+};
+
+export default CardMacro;
