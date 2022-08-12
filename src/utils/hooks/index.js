0470cb801f690ac366db81823727fc424bf8f0a6
Bryan <bryanparisot@hotmail.com>
2022-08-12 17:20:53 +0200

    :sparkles: Refract data for use data easy

--- src/utils/hooks/index.js
+++ src/utils/hooks/index.js
@@ -5,37 +5,55 @@ const BaseUrl = "http://localhost:3000";
 export function useFetch(service, userId) {
   const [data, setData] = useState([]);
 
-  const NameActivity = currentService(service, userId);
-
+  const nameActivity = currentService(service, userId);
   useEffect(() => {
+    if (!nameActivity) return;
     async function fetchData() {
       try {
-        const url = `${BaseUrl}${NameActivity}`;
-
+        const url = `${BaseUrl}${nameActivity}`;
         const response = await fetch(url);
         const data = await response.json();
-        setData(data);
+
+        const dataFormat = format(data, service);
+        setData(dataFormat);
       } catch (error) {
         console.error(error);
       }
     }
     fetchData();
-  }, [service, userId, NameActivity]);
-
+  }, [service, userId, nameActivity]);
+  
   function currentService(service, userId) {
     switch (service) {
       case "userName":
-        console.log("path userName");
+        return `/user/${userId}`;
+      case "percentageActivity":
         return `/user/${userId}`;
       case "activity":
-        console.log("path activity");
         return `/user/${userId}/activity`;
       case "averageSessions":
         return `/user/${userId}/average-sessions`;
       case "performance":
         return `/user/${userId}/performance`;
       default:
-        console.log("aaaaaa");
+        console.log("error");
+    }
+  }
+
+  function format(data, service) {
+    switch (service) {
+      case "averageSessions":
+        return data.data?.sessions;
+      case "activity":
+        return data.data?.sessions;
+      case "userName":
+        return data.data;
+      case "performance":
+        return data.data;
+      case "percentageActivity":
+        return data.data;
+      default:
+        console.log("error");
     }
   }
   return data;
